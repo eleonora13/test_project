e3c6d6b47a878aa1abfac1cfdf6318258ac656d4
Eleonora Mamedova <eleonora.mamedova13@gmail.com>
2018-10-09 17:26:16 +0300

    explicit custom wait: bbc

---
+++ js/bbc.js
@@ -0,0 +1,22 @@
+const ExplicitWait = require('../config/ExplicitWait');
+
+const URL = require('../config/URL');
+
+
+describe('Check Web Elements', function() {
+
+    it('Wait when element is displayed', async () => {
+        await browser.waitForAngularEnabled(false);
+        await browser.get(URL.bbc);
+        try {
+            const explicitWait = new ExplicitWait();
+
+            let searchField = await element(explicitWait.validElement);
+            const result = await explicitWait.elementDisplayed(searchField, 10000);
+            console.log(result);
+
+        } catch(e) {
+            console.log(e);
+        }
+    });
+});
