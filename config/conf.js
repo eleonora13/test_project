34f27d9846758e771e227e60b5c46e6faf30098f
Eleonora Mamedova <eleonora.mamedova13@gmail.com>
2018-10-23 20:42:15 +0300

    allure

--- config/conf.js
+++ config/conf.js
@@ -2,6 +2,7 @@ require('babel-core/register');
 
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect: true,
     suites: {
         assertions: '../tests/suites/assertions/*spec.js',
         bbc: '../tests/suites/bbc/*spec.js',
