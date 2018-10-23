b77e6bd68bad833b4f218f8a88abcb9e616931e9
Eleonora Mamedova <eleonora.mamedova13@gmail.com>
2018-10-23 16:10:49 +0300

    allure

--- helpers/helper_yandex.js
+++ helpers/helper_yandex.js
@@ -2,23 +2,23 @@ let until = protractor.ExpectedConditions;
 
 export const helpers = {
     waitElementPresence: async function (element) {
-        await browser.wait(until.presenceOf(element), 5000);
+        await browser.wait(until.presenceOf(element), 10000);
     },
 
     waitElementClickable: async function (element) {
-        await browser.wait(until.elementToBeClickable(element), 5000);
+        await browser.wait(until.elementToBeClickable(element), 10000);
     },
 
     waitElementVisible: async function (element) {
-        await browser.wait(until.visibilityOf(element), 5000);
+        await browser.wait(until.visibilityOf(element), 10000);
     },
 
     waitElementSelected: async function (element) {
-        await browser.wait(until.elementIsSelected(element), 5000);
+        await browser.wait(until.elementIsSelected(element), 10000);
     },
 
     waitElementNotSelected: async function (element) {
-        await browser.wait(until.elementIsNotSelected(element), 5000);
+        await browser.wait(until.elementIsNotSelected(element), 10000);
     }
 };
 
